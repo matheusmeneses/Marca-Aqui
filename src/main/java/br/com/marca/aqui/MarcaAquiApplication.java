a773ad019744ce0a723f74d6da3d8e1a88f5380c
Matheus Meneses de Souza Ferreira <mhateus1995@gmail.com>
2023-12-18 18:20:22 -0300

    primeiro commit

---
+++ src/main/java/br/com/marca/aqui/MarcaAquiApplication.java
@@ -0,0 +1,14 @@
+package br.com.marca.aqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcaAquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcaAquiApplication.class, args);
+		//TODO teste
+	}
+
+}
